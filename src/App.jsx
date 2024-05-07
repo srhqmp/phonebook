224c0760d14f125997f27f9dec32ba3932c8933a
srhqmp <ballanosarah@gmail.com>
2024-05-07 19:17:17 +0800

    feat(part2.8): implemen adding numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const onSubmit = (event) => {
     event.preventDefault();
 
-    const personExists = persons.find((p) => p.name === newName);
+    const personExists = persons.find(
+      (p) => p.name.toLowerCase().trim() === newName.toLowerCase().trim()
+    );
     if (personExists) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      const person = { name: newName };
+      const person = { name: newName, number: newNumber };
       setPersons((curr) => [...curr, person]);
       setNewName("");
+      setNewNumber("");
     }
   };
 
@@ -29,12 +35,21 @@ const App = () => {
           />
         </div>
         <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={(event) => setNewNumber(event.target.value)}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((p) => (
-        <div key={p.name}>{p.name}</div>
+        <div key={p.name}>
+          {p.name} {p.number}
+        </div>
       ))}
     </div>
   );
