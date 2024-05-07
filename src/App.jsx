285c831a5afb84bb68e851dfc2800511d6b0017d
srhqmp <ballanosarah@gmail.com>
2024-05-07 19:25:41 +0800

    feat(part2.9): implement search filter

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -16,16 +20,32 @@ const App = () => {
     if (personExists) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      const person = { name: newName, number: newNumber };
+      const person = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1,
+      };
       setPersons((curr) => [...curr, person]);
       setNewName("");
       setNewNumber("");
     }
   };
 
+  const filteredPersons = persons.filter((p) =>
+    p.name.toLowerCase().includes(search.toLocaleLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={onSubmit}>
         <div>
           name:{" "}
@@ -46,8 +66,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((p) => (
-        <div key={p.name}>
+      {filteredPersons.map((p) => (
+        <div key={p.id}>
           {p.name} {p.number}
         </div>
       ))}
