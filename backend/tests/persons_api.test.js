a72e3f1ac2bb4df5f60268295d3415d67973155b
srhqmp <ballanosarah@gmail.com>
2024-05-11 23:26:05 +0800

    refactor(backend): wrap persons test inside describe block

--- backend/tests/persons_api.test.js
+++ backend/tests/persons_api.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const { test, after, before } = require("node:test");
+const { test, after, before, describe } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
@@ -10,86 +10,88 @@ const app = require("../app.js");
 
 const api = supertest(app);
 
-before(async () => {
-  await Person.deleteMany({});
+describe("persons api", () => {
+  before(async () => {
+    await Person.deleteMany({});
 
-  await Promise.all(
-    helper.initialPersons.map(async (p) => {
-      const person = new Person(p);
-      await person.save();
-    })
-  );
-});
+    await Promise.all(
+      helper.initialPersons.map(async (p) => {
+        const person = new Person(p);
+        await person.save();
+      })
+    );
+  });
 
-test("phonebook data are returned as json", async () => {
-  await api
-    .get("/api/persons")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+  test("phonebook data are returned as json", async () => {
+    await api
+      .get("/api/persons")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 
-test("all persons are returned", async () => {
-  const response = await api.get("/api/persons");
-  assert.strictEqual(response.body.length, helper.initialPersons.length);
-});
+  test("all persons are returned", async () => {
+    const response = await api.get("/api/persons");
+    assert.strictEqual(response.body.length, helper.initialPersons.length);
+  });
 
-test("the first person is named Sarah", async () => {
-  const response = await api.get("/api/persons");
+  test("the first person is named Sarah", async () => {
+    const response = await api.get("/api/persons");
 
-  const persons = response.body.map((p) => p.name);
-  assert.strictEqual(persons[0], "Sarah");
-});
+    const persons = response.body.map((p) => p.name);
+    assert.strictEqual(persons[0], "Sarah");
+  });
 
-test("a valid number can be added", async () => {
-  const newPerson = {
-    name: "John Doe",
-    number: "12-1122334",
-  };
+  test("a valid number can be added", async () => {
+    const newPerson = {
+      name: "John Doe",
+      number: "12-1122334",
+    };
 
-  await api
-    .post("/api/persons")
-    .send(newPerson)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/persons")
+      .send(newPerson)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const personsAtEnd = await helper.personsInDb();
-  assert.strictEqual(personsAtEnd.length, helper.initialPersons.length + 1);
+    const personsAtEnd = await helper.personsInDb();
+    assert.strictEqual(personsAtEnd.length, helper.initialPersons.length + 1);
 
-  const persons = personsAtEnd.map((p) => p.name);
-  assert(persons.includes(newPerson.name));
-});
+    const persons = personsAtEnd.map((p) => p.name);
+    assert(persons.includes(newPerson.name));
+  });
 
-test("a specific person can be viewed", async () => {
-  const personsAtStart = await helper.personsInDb();
+  test("a specific person can be viewed", async () => {
+    const personsAtStart = await helper.personsInDb();
 
-  const personToView = personsAtStart[0];
+    const personToView = personsAtStart[0];
 
-  const resultPerson = await api
-    .get(`/api/persons/${personToView.id}`)
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+    const resultPerson = await api
+      .get(`/api/persons/${personToView.id}`)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
 
-  assert.deepStrictEqual(personToView, resultPerson.body);
-});
+    assert.deepStrictEqual(personToView, resultPerson.body);
+  });
 
-test("a person can be deleted", async () => {
-  const personsAtStart = await helper.personsInDb();
+  test("a person can be deleted", async () => {
+    const personsAtStart = await helper.personsInDb();
 
-  const personToDelete = personsAtStart[0];
+    const personToDelete = personsAtStart[0];
 
-  await api.delete(`/api/persons/${personToDelete.id}`).expect(204);
+    await api.delete(`/api/persons/${personToDelete.id}`).expect(204);
 
-  const personsAtEnd = await helper.personsInDb();
-  const persons = personsAtEnd.map((p) => p.name);
+    const personsAtEnd = await helper.personsInDb();
+    const persons = personsAtEnd.map((p) => p.name);
 
-  assert(!persons.includes(personToDelete.name));
-  assert.strictEqual(personsAtEnd.length, personsAtStart.length - 1);
-});
+    assert(!persons.includes(personToDelete.name));
+    assert.strictEqual(personsAtEnd.length, personsAtStart.length - 1);
+  });
 
-test("fails with statuscode 400 id is invalid", async () => {
-  const invalidId = "5a3d5da59070081a82a3445";
+  test("fails with statuscode 400 id is invalid", async () => {
+    const invalidId = "5a3d5da59070081a82a3445";
 
-  await api.get(`/api/persons/${invalidId}`).expect(400);
+    await api.get(`/api/persons/${invalidId}`).expect(400);
+  });
 });
 
 after(async () => {
