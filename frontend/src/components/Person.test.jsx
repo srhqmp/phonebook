a1d029574be2d83ee5c5ff95168ec038bfd798a2
srhqmp <ballanosarah@gmail.com>
2024-05-13 18:39:33 +0800

    test(frontend): Togglable

--- frontend/src/components/Person.test.jsx
+++ frontend/src/components/Person.test.jsx
@@ -1,14 +1,15 @@
-import { render } from '@testing-library/react'
-import Person from './Person.jsx'
+import { render } from "@testing-library/react";
 
-test('renders content', () => {
-  const person = {
-    name: 'Jane Doe',
-    number: '09-9128374',
-  }
+import Person from "./Person.jsx";
 
-  const {container} = render(<Person person={person} />)
+const person = {
+  name: "Jane Doe",
+  number: "09-9128374",
+};
 
-  const div = container.querySelector(".person")
-  expect(div).toHaveTextContent('Jane Doe 09-9128374')
-})
+test("renders content", () => {
+  const { container } = render(<Person person={person} />);
+
+  const div = container.querySelector(".person");
+  expect(div).toHaveTextContent("Jane Doe 09-9128374");
+});
